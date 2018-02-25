dae31851c149cc46638b5f2710231665dd04587f
Mona <31872911+Moni1995@users.noreply.github.com>
2018-02-25 00:53:56 -0600

    Update chapter5_exercises.hs

--- chapter5_exercises.hs
+++ chapter5_exercises.hs
@@ -1,4 +1,13 @@
--- exersise 1
+-- exercise 1
 
 pyths :: Int -> [(Int, Int, Int)]
 pyths n = [(x,y,z)|x <- [1..n],y <-[1..n],z <- [1..n], x^2 + y^2 == z^2]
+
+-- exercise 2
+factor' n = [x|x <- [1..n-1], n `mod` x == 0]
+perfect n = [x|x <- [1..n], sum (factor' x) == x]
+
+-- another way to solve it
+perfect' n = [x|x<-[1..n], sum ((\x ->[x' | x' <- [1..x-1], x `mod` x' == 0]) x) == x]
+
+-- exercise 3
